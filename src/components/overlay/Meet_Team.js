87b8cb7378a9476a46a2a879b93a9577558c70e1
Tapusa Codrin <codrinvorbaretu@yahoo.com>
2021-09-30 01:43:36 +0300

    third

--- src/components/overlay/Meet_Team.js
+++ src/components/overlay/Meet_Team.js
@@ -7,64 +7,62 @@ export default function Team(props) {
   return (
     <>
       <div className={style.overlayDiv}>
-
-      <div className={style.overlay}>
-        <Carousel fade >
-          <Carousel.Item>
-            <img  src={imageGeorge}alt="George"/>
-            <Carousel.Caption>
-              <h3 style={{ color: "black", fontSize: "55px" }}>George</h3>
-              <p
-                style={{
-                  color: "orange",
-                  fontWeight: 800,
-                  fontSize: "20px",
-                  textDecorationLine: "underline",
-                  textUnderlineOffset: "5px",
-                }}
-              >
-                Senior Front-End Specialist.
-              </p>
-            </Carousel.Caption>
-          </Carousel.Item >
-          <Carousel.Item >
-            <img src={imageMonica} alt="Monica" />  
-            <Carousel.Caption>
-              <h3 style={{ color: "black", fontSize: "55px" }}>Monica</h3>
-              <p
-                style={{
-                  color: "orange",
-                  fontWeight: 800,
-                  fontSize: "20px",
-                  textDecorationLine: "underline",
-                  textUnderlineOffset: "5px",
-                }}
-              >
-                Manager Executiv.
-              </p>
-            </Carousel.Caption>
-          </Carousel.Item>
-          <Carousel.Item >
-            <img src={imageBogdan} alt="Bogdan" />
-            <Carousel.Caption>
-              <h3 style={{ color: "black", fontSize: "55px" }}>Bogdan</h3>
-              <p
-                style={{
-                  color: "orange",
-                  fontWeight: 800,
-                  fontSize: "20px",
-                  textDecorationLine: "underline",
-                  textUnderlineOffset: "5px",
-                }}
-              >
-                Global IT Infrastructure Specialist.
-              </p>
-            </Carousel.Caption>
-          </Carousel.Item>
-        </Carousel>
+        <div className={style.overlay}>
+          <Carousel fade>
+            <Carousel.Item>
+              <img className={style.img} src={imageGeorge} alt="George" />
+              <Carousel.Caption>
+                <h3 style={{ color: "black", fontSize: "55px" }}>George</h3>
+                <p
+                  style={{
+                    color: "orange",
+                    fontWeight: 800,
+                    fontSize: "20px",
+                    textDecorationLine: "underline",
+                    textUnderlineOffset: "5px",
+                  }}
+                >
+                  Senior Front-End Specialist.
+                </p>
+              </Carousel.Caption>
+            </Carousel.Item>
+            <Carousel.Item>
+              <img src={imageMonica} className={style.img} alt="Monica" />
+              <Carousel.Caption>
+                <h3 style={{ color: "black", fontSize: "55px" }}>Monica</h3>
+                <p
+                  style={{
+                    color: "orange",
+                    fontWeight: 800,
+                    fontSize: "20px",
+                    textDecorationLine: "underline",
+                    textUnderlineOffset: "5px",
+                  }}
+                >
+                  Manager Executiv.
+                </p>
+              </Carousel.Caption>
+            </Carousel.Item>
+            <Carousel.Item>
+              <img src={imageBogdan} className={style.img} alt="Bogdan" />
+              <Carousel.Caption>
+                <h3 style={{ color: "black", fontSize: "55px" }}>Bogdan</h3>
+                <p
+                  style={{
+                    color: "orange",
+                    fontWeight: 800,
+                    fontSize: "20px",
+                    textDecorationLine: "underline",
+                    textUnderlineOffset: "5px",
+                  }}
+                >
+                  Global IT Infrastructure Specialist.
+                </p>
+              </Carousel.Caption>
+            </Carousel.Item>
+          </Carousel>
+        </div>
       </div>
-      </div>
-      
     </>
   );
 }

61842885a8ac484ceb453fa91d80636c01eee514
Tapusa Codrin <codrinvorbaretu@yahoo.com>
2021-10-01 02:25:29 +0300

    fifth

--- src/App.js
+++ src/App.js
@@ -2,15 +2,16 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import Home from "./pages/Home";
 import Servicii from "./pages/Servicii";
 import Despre from "./pages/Despre_noi";
-import ContactPage  from "../src/pages/ContactPage";
+import ContactPage from "../src/pages/ContactPage";
 import { Switch, Route } from "react-router-dom";
+import Solutii from "./pages/Solutii";
 function App() {
   return (
     <div>
       <Switch>
         <Route path="/" component={Home} exact></Route>
-        <Route path="/servicii" component={Servicii}></Route>
-        <Route path="/solutii" component={Servicii}></Route>
+        <Route exact path="/servicii" component={Servicii}></Route>
+        <Route exact path="/solutii" component={Solutii}></Route>
         <Route path="/despre_noi" component={Despre}></Route>
         <Route path="/contact" component={ContactPage}></Route>
       </Switch>
--- src/Routes/nav_bar_routes.js
+++ src/Routes/nav_bar_routes.js
@@ -3,28 +3,15 @@ import Servicii from "../pages/Servicii.js";
 import Home from "../pages/Home.js";
 import Solutii from "../pages/Solutii.js";
 import Despre from "../pages/Despre_noi.js";
-import Team from "../components/meet_the_team/Meet_Team.js";
 import ContactPage from "../pages/ContactPage.js";
 function NavigationLinks() {
   return (
     <Switch>
-      <Route path="/" exact>
-        <Home />
-      </Route>
-      <Route path="/servicii" component={Servicii} />
-
-      <Route path="/solutii">
-        <Solutii></Solutii>
-      </Route>
-      <Route path="/despre_noi">
-        <Despre></Despre>
-      </Route>
-      <Route path="/contact">
-        <ContactPage></ContactPage>
-      </Route>
-      <Route path="/team">
-        <Team></Team>
-      </Route>
+      <Route exact path="/" component={Home}></Route>
+      <Route exact path="/servicii" component={Servicii}></Route>
+      <Route exact path="/solutii" component={Solutii}></Route>
+      <Route exact path="/despre_noi" component={Despre}></Route>
+      <Route exact path="/contact" component={ContactPage}></Route>
     </Switch>
   );
 }

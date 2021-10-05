ab826de394b2b57824dc36e76633ff15b738d30b
Tapusa Codrin <codrinvorbaretu@yahoo.com>
2021-10-06 00:00:14 +0300

    deployment

--- app.js
+++ app.js
@@ -2,51 +2,65 @@ const express = require("express");
 const router = express.Router();
 const cors = require("cors");
 const nodemailer = require("nodemailer");
-require('dotenv').config()
-const app = express()
+const path = require("path");
+require("dotenv").config();
+const app = express();
 
 app.use(cors());
 app.use(express.json());
 app.use("/", router);
-const port = process.env.MAIL_PORT || 5000;
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/client/build")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+  });
+} else {
+  app.get("/", (req, res) => {
+    res.send("Api running");
+  });
+}
+
+const port = process.env.PORT || 5000;
 app.listen(port, () => console.log("Server Running on port: " + `${port}`));
 
 const contactEmail = nodemailer.createTransport({
-    service: 'gmail',
-    auth: {
-      user: `${process.env.GMAIL_USER}`,
-      pass: `${process.env.GMAIL_PASS}`,
-    },
-  });
-  
-  contactEmail.verify((error) => {
-    if (error) {
-      console.log(error);
-    } else {
-      console.log("Ready to Send");
-    }
-  });
+  service: "gmail",
+  auth: {
+    user: `${process.env.GMAIL_USER}`,
+    pass: `${process.env.GMAIL_PASS}`,
+  },
+});
 
-  router.post("/contact", (req, res) => {
-    const name = req.body.name;
-    const email = req.body.email;
-    const message = req.body.message; 
-    const mail = {
-      from: `${name}`,
-      to: `${process.env.GMAIL_USER}`,
-      subject: "Contact",
-      html: `<p>Name: ${name}</p>
+contactEmail.verify((error) => {
+  if (error) {
+    console.log(error);
+  } else {
+    console.log("Ready to Send");
+  }
+});
+
+router.post("/contact", (req, res) => {
+  const name = req.body.name;
+  const email = req.body.email;
+  const message = req.body.message;
+  const mail = {
+    from: `${name}`,
+    to: `${process.env.GMAIL_USER}`,
+    subject: "Contact",
+    html: `<p>Name: ${name}</p>
              <p>Email: ${email}</p>
              <p>Message: ${message}</p>`,
-    };
-    contactEmail.sendMail(mail, (error) => {
-      if (error) {
-        res.json({ status: "ERROR" });
-      } else {
-        res.json({ status: "Message Sent" });
-        console.log('message sent');
-      }
-    });
+  };
+  contactEmail.sendMail(mail, (error) => {
+    if (error) {
+      res.json({ status: "ERROR" });
+    } else {
+      res.json({ status: "Message Sent" });
+      console.log("message sent");
+    }
   });
+});
 
 module.exports = app;
